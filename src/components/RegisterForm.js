9417b25652a14dddd3db84ad3ed3c17544407af5
Anharu <haruan2394@gmail.com>
2019-08-23 01:31:19 +0900

    Add: Add input box

--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useState } from "react"
 
-export function RegisterForm() {
+function useRegisterForm() {
+  let [username, setUsername] = useState("")
+  let [nickname, setNickname] = useState("")
+  let [email, setEmail] = useState("")
+  let [password, setPassword] = useState("")
+  return {username, nickname, email, password, setUsername, setNickname, setEmail, setPassword}
+}
+
+export function RegisterForm(props) {
+  let data = useRegisterForm()
   return (
     <div>
-      This is Home!
+      <label>ユーザーネーム</label>
+      <input type="text" value={data.username} onChange={e => data.setUsername(e.target.value)} /> <br />
+      <label>ニックネーム</label>
+      <input type="text" value="A" /> <br />
+      <label>メールアドレスa</label>
+      <input type="email" value="A" /> <br />
+      <label>パスワード</label>
+      <input type="password" value="A" /> <br />
+
     </div>
   );
 }
