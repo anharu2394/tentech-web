32636bb6cdd35c90c55177f30aa19d09137304e1
Anharu <haruan2394@gmail.com>
2019-08-23 02:15:59 +0900

    Add: Attach state to input

--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -10,17 +10,24 @@ function useRegisterForm() {
 
 export function RegisterForm(props) {
   let data = useRegisterForm()
+  let user = {
+    username: data.username,
+    nickname: data.nickname,
+    email: data.email,
+    password: data.password
+  }
+  console.log(props)
   return (
-    <div>
+    <form onSubmit={() => props.createUser(user)}>
       <label>ユーザーネーム</label>
       <input type="text" value={data.username} onChange={e => data.setUsername(e.target.value)} /> <br />
       <label>ニックネーム</label>
-      <input type="text" value="A" /> <br />
-      <label>メールアドレスa</label>
-      <input type="email" value="A" /> <br />
+      <input type="text" value={data.nickname} onChange={e => data.setNickname(e.target.value)} /> <br />
+      <label>メールアドレス</label>
+      <input type="email" value={data.email} onChange={e => data.setEmail(e.target.value)} /> <br />
       <label>パスワード</label>
-      <input type="password" value="A" /> <br />
-
-    </div>
+      <input type="password" value={data.password} onChange={e => data.setPassword(e.target.value)} /> <br />
+      <button type="submit">登録</button>
+    </form>
   );
 }
