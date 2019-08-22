5f39ca72f23a560849e8516d976c8477eb54e8e1
Anharu <haruan2394@gmail.com>
2019-08-23 01:31:09 +0900

    Add: Add CurrentUser provider

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import React from 'react';
-import { Home } from './components/Home';
-import { Signin } from './components/Signin';
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import { useCurrentUser } from "./containers/CurrentUser";
+import React from 'react'
+import { Home } from './components/Home'
+import { Signin } from './components/Signin'
+import logo from './logo.svg'
+import './App.css'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom"
+import { useCurrentUser } from "./containers/CurrentUser"
+import { createContainer } from "unstated-next"
 
+export let CurrentUser = createContainer(useCurrentUser)
 function App() {
-  let currentUser = useCurrentUser
   return (
     <Router>
       <div>
-        <Route path="/" exact component={Home} />
-        <Route path="/signin" exact component={Signin} />
+        <CurrentUser.Provider>
+          <Route path="/" exact component={Home} />
+          <Route path="/signin" exact component={Signin} />
+        </CurrentUser.Provider>
       </div>
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
