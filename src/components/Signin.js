88a1c331def286c588f0f49bf4b89dec2b6150b6
Anharu <haruan2394@gmail.com>
2019-08-23 00:59:42 +0900

    Add: Add siginin

---
+++ src/components/Signin.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { RegisterForm} from "./RegisterForm";
+
+export function Signin() {
+  return (
+    <div>
+      This is Home!
+      <RegisterForm />
+    </div>
+  );
+}
