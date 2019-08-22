df6d2516e5e0f3b68652b9abb824c78497fa5bbb
Anharu <haruan2394@gmail.com>
2019-08-23 00:58:46 +0900

    Add: Intro react router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React from 'react';
+import { Home } from './components/Home';
+import { Signin } from './components/Signin';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { useCurrentUser } from "./containers/CurrentUser";
 
 function App() {
+  let currentUser = useCurrentUser
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Route path="/" exact component={Home} />
+        <Route path="/signin" exact component={Signin} />
+      </div>
+    </Router>
   );
 }
 
