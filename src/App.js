a745b9574169c8390a7b1067d2c8fd64df58c3af
Anharu <haruan2394@gmail.com>
2019-08-23 17:48:34 +0900

    Add: use product provider

--- src/App.js
+++ src/App.js
@@ -7,18 +7,23 @@ import logo from './logo.svg'
 import 'react-bulma-components/dist/react-bulma-components.min.css';
 import { BrowserRouter as Router, Route, Link, Redirect } from "react-router-dom"
 import { useCurrentUser } from "./containers/CurrentUser"
+import { useProduct } from "./containers/Product"
 import { createContainer } from "unstated-next"
 
 export let CurrentUser = createContainer(useCurrentUser)
+export let Product = createContainer(useProduct)
+
 function App() {
   return (
     <Router>
       <div>
         <CurrentUser.Provider>
-          <Header />
-          <Route path="/" exact component={Home} />
-          <Route path="/signin" exact component={Signin} />
-          <Route path="/login" exact component={Login} />
+          <Product.Provider>
+            <Header />
+            <Route path="/" exact component={Home} />
+            <Route path="/signin" exact component={Signin} />
+            <Route path="/login" exact component={Login} />
+          </Product.Provider>
         </CurrentUser.Provider>
       </div>
     </Router>
