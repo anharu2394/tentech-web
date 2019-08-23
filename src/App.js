64fd860d237833f59956b51041a7a6736f5b0b2a
Anharu <haruan2394@gmail.com>
2019-08-23 19:19:46 +0900

    Add: Add product componnet

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Home } from './components/Home'
 import { Header } from './components/Header'
 import { Signin } from './components/Signin'
 import { Login } from './components/Login'
+import { Product as ProductComponent } from './components/Product'
 import logo from './logo.svg'
 import 'react-bulma-components/dist/react-bulma-components.min.css';
 import { BrowserRouter as Router, Route, Link, Redirect } from "react-router-dom"
 import { useCurrentUser } from "./containers/CurrentUser"
 import { useProduct } from "./containers/Product"
 import { createContainer } from "unstated-next"
+import history from './history'
 
 export let CurrentUser = createContainer(useCurrentUser)
 export let Product = createContainer(useProduct)
