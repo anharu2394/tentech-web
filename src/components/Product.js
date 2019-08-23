29a7930148144297a9dd6327ef2a6ea2842e0b1f
Anharu <haruan2394@gmail.com>
2019-08-23 19:21:03 +0900

    Add: Add product component

---
+++ src/components/Product.js
@@ -0,0 +1,32 @@
+import React, { useEffect} from "react"
+import { CurrentUser } from "../App"
+import { Navbar, Button } from 'react-bulma-components'
+
+export function Product() {
+  let currentUser = CurrentUser.useContainer()
+  return (
+    <Navbar
+    >
+      <Navbar.Brand>
+        <Navbar.Item renderAs="a" href="#">
+          <img
+            src="https://bulma.io/images/bulma-logo.png"
+            alt="Bulma: a modern CSS framework based on Flexbox"
+            width="112"
+            height="28"
+          />
+        </Navbar.Item>
+        <Navbar.Burger
+        />
+      </Navbar.Brand>
+      <Navbar.Menu >
+        <Navbar.Container>
+          <Navbar.Item href="#">Second</Navbar.Item>
+        </Navbar.Container>
+        <Navbar.Container position="end">
+          <Navbar.Item href="#">At the end</Navbar.Item>
+        </Navbar.Container>
+      </Navbar.Menu>
+    </Navbar>
+  )
+}
