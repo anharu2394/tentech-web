23d406d1a9228a0a109e53d98b7f38abd17f3a05
Anharu <haruan2394@gmail.com>
2019-08-24 07:23:56 +0900

    Add: selierlize html

--- src/components/CreateProductForm.js
+++ src/components/CreateProductForm.js
@@ -182,12 +182,32 @@ export const CreateProductForm = withRouter((props)  => {
               return next()
       }
 	}
+  const renderBlock = (props, editor, next) => {
+    const { attributes, children, node } = props
+
+    switch (node.type) {
+      case 'block-quote':
+        return <blockquote {...attributes}>{children}</blockquote>
+      case 'bulleted-list':
+        return <ul {...attributes}>{children}</ul>
+      case 'heading-one':
+        return <h1 {...attributes}>{children}</h1>
+      case 'heading-two':
+        return <h2 {...attributes}>{children}</h2>
+      case 'list-item':
+        return <li {...attributes}>{children}</li>
+      case 'numbered-list':
+        return <ol {...attributes}>{children}</ol>
+      default:
+        return next()
+    }
+  }
   let data = useProductForm(initialValue)
   console.log(props)
   const { history, location, match } = useReactRouter()
   let req_data = {
     title: data.title,
-    body: data.body,
+    body: html.serialize(data.body),
     img: data.img,
     duration: Number(data.duration),
     kind: data.kind,
