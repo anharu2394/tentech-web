bd645fb0cec96a98f1d83242547e551ccfeffc1b
Anharu <haruan2394@gmail.com>
2019-08-23 22:19:19 +0900

    Add: import Footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Home } from './components/Home'
 import { Header } from './components/Header'
+import { Footer } from './components/Footer'
 import { Signin } from './components/Signin'
 import { Login } from './components/Login'
 import { Product as ProductComponent } from './components/Product'
