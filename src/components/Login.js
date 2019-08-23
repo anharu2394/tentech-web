3fcf1d18e65b3ddfc4a903438de52149f2b05a0d
Anharu <haruan2394@gmail.com>
2019-08-23 16:03:28 +0900

    Add: Add login check

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,14 +1,18 @@
 import React from "react"
 import { LoginForm} from "./LoginForm"
 import { CurrentUser } from "../App"
+import { Redirect } from "react-router-dom"
 
 export function Login(props) {
   let currentUser = CurrentUser.useContainer()
   console.log(currentUser)
+  if (currentUser.loggedIn) {
+    return <Redirect to="/" />
+  }
   return (
     <div>
       Login!
       <LoginForm loginUser={currentUser.loginUser}/>
     </div>
-  );
+  )
 }
