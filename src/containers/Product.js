920800521097f0056621c1401a41ab1165e73da9
Anharu <haruan2394@gmail.com>
2019-08-24 02:16:14 +0900

    Add: set inital state

--- src/containers/Product.js
+++ src/containers/Product.js
@@ -2,8 +2,8 @@ import React, { useState } from "react"
 import { endpoint } from "../utils"
 
 export function useProduct() {
-	let [ product, setProduct ] = useState({})
-	let [ author, setAuthor ] = useState({})
+	let [ product, setProduct ] = useState({title:"", body:""})
+	let [ author, setAuthor ] = useState({nickname:"",username:""})
   let createProduct = (product,token) => {
     const data = { "product": product }
     return fetch(endpoint("/products"),{
