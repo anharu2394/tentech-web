d44556f8a968f189ce4f2822d80badb2e45baa33
Anharu <haruan2394@gmail.com>
2019-08-23 17:05:17 +0900

    Add: Add hero on home

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,32 @@
 import React, { useEffect} from "react"
 import { CurrentUser } from "../App"
+import { Navbar, Button } from 'react-bulma-components'
 
 export function Header() {
   let currentUser = CurrentUser.useContainer()
   return (
-    <div>
-      This is Home!
-    </div>
+    <Navbar
+    >
+      <Navbar.Brand>
+        <Navbar.Item renderAs="a" href="#">
+          <img
+            src="https://bulma.io/images/bulma-logo.png"
+            alt="Bulma: a modern CSS framework based on Flexbox"
+            width="112"
+            height="28"
+          />
+        </Navbar.Item>
+        <Navbar.Burger
+        />
+      </Navbar.Brand>
+      <Navbar.Menu >
+        <Navbar.Container>
+          <Navbar.Item href="#">Second</Navbar.Item>
+        </Navbar.Container>
+        <Navbar.Container position="end">
+          <Navbar.Item href="#">At the end</Navbar.Item>
+        </Navbar.Container>
+      </Navbar.Menu>
+    </Navbar>
   )
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,22 @@
 import React, { useEffect} from "react"
+import { Section, Hero, Container, Heading } from 'react-bulma-components'
 
 export function Home() {
   return (
     <div>
+      <Hero color="primary" size="medium">
+        <Hero.Body>
+          <Container>
+            <Heading>あなたのプロジェクトを投稿しよう！</Heading>
+            <Heading subtitle size={4}>
+              Webアプリ、モバイルアプリ、ライブラリ、なんでも投稿OK
+            </Heading>
+            <Heading subtitle size={4}>
+              どんなに小さいプロジェクトでもまずは投稿するのが第一歩
+            </Heading>
+          </Container>
+        </Hero.Body>
+      </Hero>
       This is Home!
     </div>
   )
