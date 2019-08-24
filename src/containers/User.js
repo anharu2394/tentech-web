476945e557c354266bdd720cbc5acf6e90cb308d
Anharu <haruan2394@gmail.com>
2019-08-24 18:53:05 +0900

    Add: Add User Container

---
+++ src/containers/User.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react"
+import { createContainer } from "unstated-next"
+import { render } from "react-dom"
+import { endpoint} from "../utils"
+
+const initialUser = {
+  "activated": false,
+  "activated_at": null,
+  "email": "",
+  "id": 0,
+  "nickname": "",
+  "username": ""
+}
+export function useUser() {
+  let [user, setUser] = useState(initialUser)
+  let [products, setProducts] = useState([])
+  let reset = () => {
+    setUser({})
+  }
+  let fetchUser = async (username) => {
+    return fetch(endpoint("/users/" + username), {
+        mode: "cors",
+    }
+    )
+    .then(res => {
+      if (res.ok) {
+        return res.json()
+      }
+    })
+    .then(j => {
+      setUser(j.user)
+      return j.user
+    })
+  } 
+  let fetchProductsByUser = async (user_id) => {
+    return fetch(endpoint("/users/" + user_id + "/products"), {
+        mode: "cors",
+    }
+    )
+    .then(res => {
+      if (res.ok) {
+        return res.json()
+      }
+    })
+    .then(j => {
+      setProducts(j.products) 
+      return j.products
+    })
+  } 
+  return { user, fetchUser, fetchProductsByUser, products}
+}
