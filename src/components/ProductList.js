31a6394d45b1f58327da1454449be0e52ff9a587
Anharu <haruan2394@gmail.com>
2019-08-24 18:51:30 +0900

    Add: Add products

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,16 +1,30 @@
 import React, { useEffect} from "react"
 import { CurrentUser } from "../App"
-import { Navbar, Button, Image } from 'react-bulma-components'
+import { Navbar, Button, Image, Card, Content, Media, Heading, Columns } from 'react-bulma-components'
 import styled from 'styled-components'
 
 export function ProductList(props) {
   let currentUser = CurrentUser.useContainer()
+  console.log(props)
   useEffect(() => {
   },[])
   return (
-    <div>
-      
-    </div>
+    <Columns>
+      {
+        props.products.map(p => (
+          <Columns.Column size={4}>
+            <Card key={p.id}>
+              <Card.Image size="4by3" src="http://bulma.io/images/placeholders/1280x960.png" />
+              <Card.Content>
+                <Content>
+                  <Heading>{p.title}</Heading>
+                </Content>
+              </Card.Content>
+            </Card>
+          </Columns.Column>
+        ))
+      } 
+    </Columns>
   )
 }
 
