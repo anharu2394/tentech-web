b3ea1b80e6126d5f7fafb9e5d374299512efa816
Anharu <haruan2394@gmail.com>
2019-08-25 15:27:48 +0900

    Add: Add recreate Product

--- src/containers/Product.js
+++ src/containers/Product.js
@@ -19,6 +19,20 @@ export function useProduct() {
     .then(r => r.json())
     .catch(e => console.log(e))
   } 
+  let recreateProduct = (product,token) => {
+    const data = { "product": product }
+    return fetch(endpoint("/products"),{
+      method: "PATCH",
+      mode: "cors",
+      headers: {
+          "Content-Type": "application/json; charset=utf-8",
+          "x-api-key": token
+      },
+      body: JSON.stringify(data)
+    })
+    .then(r => r.json())
+    .catch(e => console.log(e))
+  } 
 	let fetchProduct = (uuid) => {
     return fetch(endpoint("/products/" + uuid),{
       mode: "cors",
