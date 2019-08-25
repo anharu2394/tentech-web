d21aa00f94890ddbdb0067c6bdd20ecc1be3a51d
Anharu <haruan2394@gmail.com>
2019-08-25 15:28:21 +0900

    Add: Add Edit PRoduct

---
+++ src/components/EditProduct.js
@@ -0,0 +1,32 @@
+import React, { useEffect} from "react"
+import { Section, Hero, Container, Heading } from 'react-bulma-components'
+import { CreateProductForm } from "./CreateProductForm";
+import { Product, CurrentUser } from "../App"
+
+export function EditProduct(props) {
+  let product = Product.useContainer()
+  let user = CurrentUser.useContainer()
+  useEffect(() => {
+    product.fetchEditProduct(props.match.params.uuid)
+  },[])
+  return (
+    <div>
+      <Hero color="primary" size="medium">
+        <Hero.Body>
+          <Container>
+            <Heading>あなたのプロジェクトを投稿しよう！</Heading>
+            <Heading subtitle size={4}>
+              Webアプリ、モバイルアプリ、ライブラリ、なんでも投稿OK
+            </Heading>
+            <Heading subtitle size={4}>
+              どんなに小さいプロジェクトでも投稿するのが第一歩
+            </Heading>
+          </Container>
+        </Hero.Body>
+      </Hero>
+			<Container>
+      <CreateProductForm createProduct={product.recreateProduct} token={user.token} editProduct={product.editProduct} />
+			</Container>
+    </div>
+  )
+}
