4e4bfc17f6374bae556937e5235115af9d8f1169
Anharu <haruan2394@gmail.com>
2019-10-12 23:43:50 +0900

    Change: use object

--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -4,56 +4,66 @@ import { Field, Label, Input, Checkbox }from 'react-bulma-components/lib/compone
 import Notification from 'react-bulma-components/lib/components/notification'
 import Button from 'react-bulma-components/lib/components/button'
 function useRegisterForm() {
+  let [inputs, setInputs] = useState({})
   let [username, setUsername] = useState("")
   let [nickname, setNickname] = useState("")
   let [email, setEmail] = useState("")
   let [password, setPassword] = useState("")
-	let [accepted, setAccepted] = useState(false)
+  let [accepted, setAccepted] = useState(false)
   let [error, setError] = useState("")
-  return {username, nickname, email, password, setUsername, setNickname, setEmail, setPassword, accepted, setAccepted, error, setError}
+  const handleInputChange = (event) => {
+	event.persist();
+	if (event.target.name === "accepted") {
+		setInputs(inputs => ({...inputs, [event.target.name]: event.target.checked}))
+	}
+	else{
+		setInputs(inputs => ({...inputs, [event.target.name]: event.target.value}))
+	}
+  }
+  return {handleInputChange, inputs, setInputs, username, nickname, email, password, setUsername, setNickname, setEmail, setPassword, accepted, setAccepted, error, setError}
 }
 
 export function RegisterForm(props) {
-  let data = useRegisterForm()
+  let {inputs, setError, error, handleInputChange} = useRegisterForm()
   let user = {
-    username: data.username,
-    nickname: data.nickname,
-    email: data.email,
-    password: data.password
+    username: inputs.username,
+    nickname: inputs.nickname,
+    email: inputs.email,
+    password: inputs.password
   }
   return (
     <form onSubmit={e => {
         e.preventDefault()
-				if (data.accepted) {
-        	props.createUser(user).catch(() => data.setError("入力情報が条件を満たしていません"))
+				if (inputs.accepted) {
+        	props.createUser(user).catch(() => setError("入力情報が条件を満たしていません"))
 				} else {
-					data.setError("利用規約に同意してください")
+					setError("利用規約に同意してください")
 				}
     }
     }>
-			{ !(data.error === "") && <Notification color="danger">{data.error}</Notification>}
+			{ !(error === "") && <Notification color="danger">{error}</Notification>}
 			<Field>
 				<Label>ユーザーネーム</Label>
-				<Input type="text" value={data.username} onChange={e => data.setUsername(e.target.value)} /> <br />
+				<Input type="text" value={inputs.username} name="username" onChange={handleInputChange} /> <br />
 				<p>※英数字のみ可能</p>
 				<p>※1文字以上</p>
 			</Field>
 			<Field>
 				<Label>ニックネーム</Label>
-				<Input type="text" value={data.nickname} onChange={e => data.setNickname(e.target.value)} /> <br />
+				<Input type="text" value={inputs.nickname} name="nickname" onChange={handleInputChange} /> <br />
 				<p>※1文字以上</p>
 			</Field>
 			<Field>
 				<Label>メールアドレス</Label>
-				<Input type="email" value={data.email} onChange={e => data.setEmail(e.target.value)} /> <br />
+				<Input type="email" value={inputs.email} name="email" onChange={handleInputChange} /> <br />
 			</Field>
 			<Field>
 				<Label>パスワード</Label>
-				<Input type="password" value={data.password} onChange={e => data.setPassword(e.target.value)} /> <br />
+				<Input type="password" value={inputs.password} name="password" onChange={handleInputChange} /> <br />
 				<p>※8文字以上</p>
 			</Field>
 			<Field>
-				<Checkbox checked={data.accepted} onChange={e => data.setAccepted(e.target.checked)}>利用規約に同意する。</Checkbox>
+				<Checkbox checked={inputs.accepted} name="accepted" onChange={handleInputChange}>利用規約に同意する。</Checkbox>
 			</Field>
 			<Field>
 				<Button color="info" fullwidth={true} type="submit">登録</Button>
