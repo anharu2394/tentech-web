448c11e08c3147f659314277e6605ef130b14ce5
Anharu <haruan2394@gmail.com>
2019-10-12 18:23:48 +0900

    Change: Change components path on Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,9 @@
 import React, { useEffect} from "react"
-import { Navbar, Content, Container, Footer as BulmaFooter, Image, Columns } from 'react-bulma-components'
-
+import Content from 'react-bulma-components/lib/components/content'
+import BulmaFooter from 'react-bulma-components/lib/components/footer'
+import Container from 'react-bulma-components/lib/components/container'
+import Image from 'react-bulma-components/lib/components/image'
+import Columns from 'react-bulma-components/lib/components/columns'
 export function Footer() {
   return (
     <BulmaFooter>
